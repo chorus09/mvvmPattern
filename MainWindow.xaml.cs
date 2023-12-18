274ed7cfdb6d436b4756067de8ff44a86b65dab3
chorus09 <thetrumer@gmail.com>
2023-12-19 01:55:47 +0200

    Added ViewModel Folder, and design in xaml file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,11 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using mvvm.ViewModel;
 
 namespace mvvm;
 public partial class MainWindow : Window
 {
     public MainWindow() {
         InitializeComponent();
-
-    }
-    private void OnCloseThisWindow_Click(object sender, RoutedEventArgs e) {
-        Close();
+        DataContext = new MainWndViewModel();
     }
 }
---
+++ ViewModel/MainWndViewModel.cs
@@ -0,0 +1,36 @@
+﻿using mvvm.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace mvvm.ViewModel
+{
+    public class MainWndViewModel : INotifyPropertyChanged {
+        public MainWndViewModel() {
+            Cars = new ObservableCollection<Car>() {
+                new Car("Toyota", 3.0, 329, 40500),
+                new Car("BMW", 2.5, 184, 6500),
+                new Car("Mercedes", 4.0, 320, 22000),
+                new Car("Dacia", 1.2, 50, 3000)
+            };
+        }
+        private Car? _selectedCar;
+
+        public Car? SelectedCar {
+            get => _selectedCar;
+            set {
+                if(value == _selectedCar) return;
+                _selectedCar = value;
+                OnPropertyChanged(nameof(SelectedCar));
+            }
+        }
+
+        public ObservableCollection<Car>? Cars { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
